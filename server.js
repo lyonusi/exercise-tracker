f67519cc090abafba0622261f05639c02a49dcdc
Li Xiang <lixiang@Little-Silver.hitronhub.home>
2022-04-02 19:12:11 -0600

    models

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const handler = require('./src/handler/handler');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const mongoUri = process.env['MONGO_URI'];
+mongoose.connect(mongoUri, { useNewUrlParser: true });
 
-app.use(cors())
-app.use(express.static('public'))
+const db = mongoose.connection;
+db.once('open', _ => {
+  console.log('Database connected.')
+});
+
+db.on('error', err => {
+  console.error('connection error:', err)
+});
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
+app.post('/api/users', (req,res) =>{
+  handler.createUser(req,res);
+});
 
 
 
---
+++ src/handler/handler.js
@@ -0,0 +1,7 @@
+const handler = require('../db/model')
+
+const createUser = (req, res) => {
+    res.send('hello');
+}
+
+module.exports = handler;
